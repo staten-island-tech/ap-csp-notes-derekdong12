ea90d94de0d973eed006f335e0c4018e41756718
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-09-23 22:50:43 -0400

    HW#2

---
+++ .github/app.js
@@ -0,0 +1,11 @@
+const life = 100; // life bar numbers
+life = life - 10;
+life = life  / 10;
+
+var name = "Hello World"; // name
+
+var checkout = true;
+
+var box; // var without value, undefined, var box = null; // value of nothing      
+
+console.log(life);
---
+++ .github/app2.js
@@ -0,0 +1,14 @@
+const name = "Derek";
+
+function adder (num1, num2) {
+    console.log (num1 + num2);
+}
+
+adder (5, 10);
+
+function toUpper (text) {
+    const upperCased = text.toUpperCase();
+    console.log (upperCased);
+}
+
+toUpper (name);
