be175c07e28608d47befe9169060307dc3a0e185
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-09-23 23:14:20 -0400

    Hw#2

---
+++ .github/app3.js
@@ -0,0 +1,7 @@
+const name = "Derek"; 
+const age = 17;
+const combined = name + age;
+
+console.log(`Hello it's me and my name is ${name} and my age is ${age}`); //string concatenation
+
+console.log(typeof combined); //tell us the type of data in variable
