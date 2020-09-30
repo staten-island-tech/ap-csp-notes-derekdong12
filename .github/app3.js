b4d22031025a3dc4c48cd25c24a83b5a8ce5ddcb
derekdong12 <50589954+derekdong12@users.noreply.github.com>
2020-09-30 10:05:53 -0400

    HW#3

--- .github/app3.js
+++
@@ -1,7 +0,0 @@
-const name = "Derek"; 
-const age = 17;
-const combined = name + age;
-
-console.log(`Hello it's me and my name is ${name} and my age is ${age}`); //string concatenation
-
-console.log(typeof combined); //tell us the type of data in variable
---
+++ app.js
@@ -0,0 +1,39 @@
+//if else statements
+const age = 16;
+
+if(age === 18) {  //define condition, using triple equals to prevent strings being compared to the number
+    console.log() 
+}  else if(age < 15) {
+    console.log()
+}  else {
+    console.log()
+}
+
+const dice1 = 6;
+const dice2 = 3;
+
+if(dice1 === 6 || dice2 === 6) { //&& is both, || is one of the two
+    console.log()
+}  else {
+    console.log()
+}
+
+//objects
+const user = {
+    name:
+    age:
+    married:
+    purchases: [],
+
+    sayName: function() {
+        console.log(this.name);
+    }
+};
+
+user,sayName();
+
+function saymyAge(()
+
+}
+
+saymyAge();
